4a50331ba593f636bc6a2e2f13b23b4f2c43cbd6
Amir Velasquez <amir.velasquez1@gmail.com>
2022-01-10 19:14:40 -0400

    Added margin for footer

--- src/index.js
+++ src/index.js
@@ -3,22 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-    <footer style={{ position: 'absolute', top: '0', left: '0' }}>
-				Made by{' '}
-				<a
-					href="https://www.linkedin.com/in/amirvelasquez1/"
-					target="_blank"
-          rel="noreferrer"
-					style={{ color: 'greenyellow' }}
-				>
-					Amir Velasquez
-				</a>{' '}
-				for the MLH Build event
-			</footer>
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<App />
+		<footer style={{ position: 'absolute', top: '0', left: '0', margin: '20px' }}>
+			Made by{' '}
+			<a
+				href="https://www.linkedin.com/in/amirvelasquez1/"
+				target="_blank"
+				rel="noreferrer"
+				style={{ color: 'greenyellow' }}
+			>
+				Amir Velasquez
+			</a>{' '}
+			for the MLH Build event
+		</footer>
+	</React.StrictMode>,
+	document.getElementById('root')
 );
